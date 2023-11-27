34868d841959b02a2745e30e400ff015228579c7
ricko caesar putra <ricko.caesar.putra@gmail.com>
2023-11-28 00:04:43 +0700

    add jail monkey

--- src/presentation/components/atomicDesign/pages/login/Login.tsx
+++ src/presentation/components/atomicDesign/pages/login/Login.tsx
@@ -4,7 +4,6 @@ import {useDispatch} from 'react-redux';
 import AuthTemplate from '../../templates/authTemplate/AuthTemplate';
 import LoginComponent from '../../organisms/loginComponent/LoginComponent';
 import di from '@core/di';
-import {TextComponent} from '../../atoms/text/textComponent';
 
 const Login: React.FC = () => {
   const dispatch = useDispatch();
